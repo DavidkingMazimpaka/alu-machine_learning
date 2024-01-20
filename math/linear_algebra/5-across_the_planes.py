d9cc6fae46c596bf5bd4544d55ae8b387c3d1ee3
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-01-20 17:08:11 +0200

    new push

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,27 +3,14 @@
  This program add two matrices
 '''
 def add_matrices2D(mat1, mat2):
-    ''' 
-    function that add two matrices
     '''
-    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+    This function computes two matrices
+    of same length and return the sums element wise
+    '''
+    if (len(mat1) != len(mat2)) or (len(mat1[0]) != len(mat2[0])):
         return None
-
-    result = []
-
-    for i in range(len(mat1)):
-        row_result = []
-        for j in range(len(mat1[0])):
-            row_result.append(mat1[i][j] + mat2[i][j])
-        result.append(row_result)
-
-    return result
-
-
-if __name__ == "__main__":
-    mat1 = [[1, 2], [3, 4]]
-    mat2 = [[5, 6], [7, 8]]
-    print(add_matrices2D(mat1, mat2))
-    print(mat1)
-    print(mat2)
-    print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
+    matrix = [[] for _ in range(len(mat1))]
+    for row in range(len(mat1)):
+        for col in range(len(mat1[row])):
+            matrix[row].append(mat1[row][col]+mat2[row][col])
+    return matrix
