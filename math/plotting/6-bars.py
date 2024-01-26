48d442b3c7c5e6a40d0c1304957ead4b576ab577
DavidkingMazimmpaka <d.mazimpaka@alustudent.com>
2024-01-26 02:41:41 +0200

    file 6-bars modification

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -5,11 +5,17 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 fruit = np.random.randint(0, 20, (4,3))
 people = ['Farrah', 'Fred', 'Felicia']
+y_axis = range(0, 81, 10)
+y_pos = range(len(y_axis))
 
 plt.title("Number of Fruit per Person")
 # replacing x labels
 positions = range(len(people))
 plt.xticks(positions, people)
+
+# Setting y-axis range and ticks
+plt.ylim((0, 80))
+
 plt.ylabel("Number of fruits")
 plt.bar(range(3), fruit[0], color='r', label='apples')
 plt.bar(range(3), fruit[1], color='yellow', label='bananas', bottom=fruit[0]+1)
