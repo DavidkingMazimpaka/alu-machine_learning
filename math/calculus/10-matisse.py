d7d1003f54980b949fb1434d249cb142355f3c92
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-02-02 17:53:37 +0200

    10 matisse

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
+""" The derivative of polynomial
 """
-derivative of a polynomial """
 
 
 def poly_derivative(poly):
-    """function that calculates the derivative of a polynomial"""
-    deri_val = []
+    """ Find the derivative of polynomial
+    """
+    derivative = []
     if not isinstance(poly, list) or len(poly) == 0:
         return None
     if len(poly) == 1:
         return [0]
-    for i in range(1, len(poly)-1, 0, -1):
-        deri_val.append(poly[i] * i)
-    return deri_val[::-1]
+    for i in range(len(poly)-1, 0, -1):
+
+        derivative.append(poly[i]*i)
+    return derivative[::-1]
 
 
-#print(poly_derivative([0, 1, 2, 3, 4, 5]))
+# print(poly_derivative([]))
