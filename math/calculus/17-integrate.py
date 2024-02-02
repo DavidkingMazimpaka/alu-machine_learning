780b685a3304557a6cce91a9d2d688ee622fa2ea
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-02-02 18:13:42 +0200

    final commit

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,41 +1,31 @@
 #!/usr/bin/env python3
-
 """
-Function: poly_integral(poly, C=0)
-
-Calculates the integral of a polynomial.
-
+Calculates the integral of a polynomial
 """
+
 
 def poly_integral(poly, C=0):
+    """ Calculates the integral of a polynomial
     """
-    Calculates the integral of a polynomial.
-
-    Parameters:
-        poly (list): A list of coefficients representing a polynomial. The index of the list represents the power of x that the coefficient belongs to.
-                     Example: if f(x) = x^3 + 3x + 5, poly is equal to [5, 3, 0, 1].
-        C (int, optional): An integer representing the integration constant. Default is 0.
-
-    Returns:
-        list: A new list of coefficients representing the integral of the polynomial. The returned list is as small as possible.
-
-    Notes:
-        - If a coefficient is a whole number, it should be represented as an integer.
-        - If poly or C are not valid, return None.
-    """
-    # Check if poly is a valid list of coefficients
+    integral = []
+    # check for the validity of the inputs
     if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
         return None
+    # calculate the cofficients
     for i in range(len(poly)-1, 0, -1):
         integral.append(poly[i]/(i+1))
 
     integral.append(poly[0])
     integral.append(C)
-
+    # fit the output like on the intranet
     if len(poly) == 1 and poly[0] == 0:
         integral = [C]
     for i in range(len(integral)):
-        if integral[i] % 1== 0:
+        if integral[i] % 1 == 0:
             integral[i] = int(integral[i])
 
     return integral[::-1]
+
+
+# poly = [0]
+# print(poly_integral(poly, C=9))
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 """
-calculates the sum of squared elements
+Summation of squared values
 """
 
 
 def summation_i_squared(n):
-    """summation of i
-    
-    Keyword arguments:
-    Return: return None
+    """ Sum i squared
+
+    Args:
+        n (_type_): _description_
     """
     if n == 1:
         return 1
     if n < 1:
         return None
     else:
-        answer = (n*(n+1)*(2*n+1))//6
-        return answer
-    
+        result = (n*(n+1)*(2*n+1))//6
+        return result
 
-#print(summation_i_squared(5))
+
+# print(summation_i_squared(5))
     
