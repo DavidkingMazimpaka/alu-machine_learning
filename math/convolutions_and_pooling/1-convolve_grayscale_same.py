ac50826bd163698325f43de40ed8b86541526374
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-02-14 20:04:47 +0200

    1-convolve_grayscale_same

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -9,7 +9,8 @@ def convolve_grayscale_same(images, kernel):
     kh, kw = kernel.shape
     ph = max((kh - 1) // 2, kh // 2)
     pw = max((kw - 1) // 2, kw // 2)
-    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 
+                           mode='constant')
     output_h = h
     output_w = w
     output = np.zeros((m, output_h, output_w))
