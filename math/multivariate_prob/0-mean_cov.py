d2914118b9f2e30440813f4f714e69f78e7a1907
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-03-14 18:43:53 +0200

    final commit

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-""" Multivariate Probability module """
+"""mulitvaritive probability"""
 import numpy as np
 
 
 def mean_cov(X):
-    """ Calculates the mean and covariance of a data set """
+    """that calculates the mean and covariance of a data set"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        raise TypeError("X must be a 2D numpy.ndarray")
+        raise TypeError('X must be a 2D numpy.ndarray')
     n, d = X.shape
     if n < 2:
         raise ValueError("X must contain multiple data points")
-    mean = np.mean(X, axis=0, keepdims=True)
-    cov = np.matmul(X - mean).T, (X - mean) / (n - 1)
-    return mean, cov
+    m = np.mean(X, axis=0, keepdims=True)
+    C = np.matmul((X - m).T, (X - m)) / (n - 1)
+    return m, C
