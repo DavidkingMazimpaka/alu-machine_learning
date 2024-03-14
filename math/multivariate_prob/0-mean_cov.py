14896d78629cca07486a15d8803c1eea228ed40a
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-03-14 18:26:26 +0200

    error check1

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -9,7 +9,7 @@ def mean_cov(X):
         raise TypeError("X must be a 2D numpy.ndarray")
     n, d = X.shape
     if n < 2:
-        raise ValueError("X must be a 2D numpy.ndarray with at least 2 rows")
+        raise ValueError("X must contain multiple data points")
     mean = np.mean(X, axis=0, keepdims=True)
     cov = np.matmul(X - mean).T, (X - mean) / (n - 1)
     return mean, cov
