15eac733fb983efd9a467a8fbc92d030bc8bf520
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-05-31 01:46:18 +0200

    L2 reg cost 0error

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module provides a function to calculate the cost of a neural network with L2 regularization.
+a function to calculate the cost of a neural network with L2 regularization.
 """
 
 
@@ -13,7 +13,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Returns:
     float: The cost of the network accounting for L2 regularization.
     """
-    
     l2_norm_sum = 0
     for i in range(1, L + 1):
         weight_key = 'W' + str(i)
