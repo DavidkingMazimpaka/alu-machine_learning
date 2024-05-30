9d4fbdd0f11c657eb578b9e909104630341c5865
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-05-31 01:43:30 +0200

    L2 reg erro 0

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -17,7 +17,8 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     l2_norm_sum = 0
     for i in range(1, L + 1):
         weight_key = 'W' + str(i)
-        l2_norm_sum += np.sum(np.square(weights[weight_key]))
+        l2_norm_sum += np.sum(np.square
+                              (weights[weight_key]))
     l2_reg_term = (lambtha / (2 * m)) * l2_norm_sum
     l2_reg_cost = cost + l2_reg_term
     return l2_reg_cost
