d0a7991c71232132103efd3c0e977bfe0ecf4033
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-05-31 02:43:32 +0200

    task4 error=0

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-"""a function that calculates the forward propagation with dropout"""
+""" Forward Propagation with Dropout """
 
 
 import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
-    """
-    Calculating the forward propagation with dropout
-    Args:
-        X: numpy.ndarray with shape (nx, m) that contains the input data
-        nx: the number of input features to the neuron
-        m: the number of data points
-        weights: dictionary of the weights and biases of the NN
-        L: the number of layers
+    """ Forward Propagation with Dropout
+        X: (nx, m) input data
+          nx: number of input features
+          m: number of examples
+        weights: dictionary of weights and biases of the neural network
+        L: number of layers in the network
         keep_prob: probability that a node will be kept
+        Returns: dictionary containing the outputs of each
+        layer and the dropout mask used on each layer
     """
     cache = {}
-    cache["A0"] = X
+    cache['A0'] = X
     for i in range(1, L + 1):
         W = weights['W' + str(i)]
         b = weights['b' + str(i)]
@@ -32,5 +32,5 @@ def dropout_forward_prop(X, weights, L, keep_prob):
             cache['D' + str(i)] = D
         else:
             A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
-            cache['A' + str(i)] = A
+        cache['A' + str(i)] = A
     return cache
