6c385bb460afed9ab01e69e941c8feb7d0d1ce95
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-05-31 02:53:46 +0200

    task 5 erro=0

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -11,20 +11,15 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     using gradient descent with dropout
     """
     m = Y.shape[1]
-    dZ = cache["A" + str(L)] - Y
-    for l in range(L, 0, -1):
-        A_prev = cache["A" + str(l - 1)] if l > 1 else cache["A0"]
-        W = weights["W" + str(l)]
-        b = weights["b" + str(l)]
-        D = cache["D" + str(l)]
-        dA = np.dot(W.T, dZ)
-        dA *= D  # Apply dropout mask
-
-        if l > 1:
-            dA /= keep_prob  # Scale the gradients to account for dropout during training
-
-        dZ = dA * (1 - np.square(A_prev))  # Derivative of tanh activation
-        dW = (1 / m) * np.dot(dZ, A_prev.T)
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-        weights["W" + str(l)] = W - alpha * dW
-        weights["b" + str(l)] = b - alpha * db
+    dz = cache['A' + str(L)] - Y
+    for i in range(L, 0, -1):
+        A = cache['A' + str(i - 1)]
+        W = weights['W' + str(i)]
+        dW = np.matmul(dz, A.T) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
+        if i > 1:
+            dz = np.matmul(W.T, dz) * (1 - np.square(A)) * \
+                cache['D' + str(i - 1)]
+            dz = dz / keep_prob
+        weights['W' + str(i)] = weights['W' + str(i)] - alpha * dW
+        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
