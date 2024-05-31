d9f3ae6750b4a9985e441e68c63578c61166009f
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-05-31 02:06:24 +0200

    new commit

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -5,6 +5,7 @@ a function to calculate the cost of a NN with L2 regularization.
 
 
 import numpy as np
+import tensorflow as tf
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
@@ -12,18 +13,19 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Calculates the cost of a neural network with L2 regularization.
 
     Parameters:
-    cost (float): The cost of the network without L2 regularization.
+    cost (tf.Tensor): The cost of the network without L2 regularization.
     lambtha (float): The regularization parameter.
-    weights (dict): A dictionary of the weights and biases of the NN.
-    L (int): The number of layers in the NN.
+    weights (dict): A dictionary of the weights and biases of the neural network.
+    L (int): The number of layers in the neural network.
     m (int): The number of data points used.
+
     Returns:
-    float: The cost of the network accounting for L2 regularization.
+    tf.Tensor: The cost of the network accounting for L2 regularization.
     """
-    l2_norm_sum = 0
+    l2_norm_sum = tf.constant(0.0)
     for i in range(1, L + 1):
         weight_key = 'W' + str(i)
-        l2_norm_sum += np.sum(np.square(weights[weight_key]))
+        l2_norm_sum += tf.reduce_sum(tf.square(weights[weight_key]))
     l2_reg_term = (lambtha / (2 * m)) * l2_norm_sum
     l2_reg_cost = cost + l2_reg_term
     return l2_reg_cost
