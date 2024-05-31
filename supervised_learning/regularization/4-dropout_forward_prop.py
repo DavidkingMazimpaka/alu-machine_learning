e6714508d21ddd17711c4fb0b481cc82a6a282ba
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-05-31 02:41:15 +0200

    task 4 erro=0

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -19,18 +19,18 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     cache = {}
     cache["A0"] = X
     for i in range(1, L + 1):
-        W = weights["W" + str(i)]
-        b = weights["b" + str(i)]
-        A = cache["A" + str(i - 1)]
+        W = weights['W' + str(i)]
+        b = weights['b' + str(i)]
+        A = cache['A' + str(i - 1)]
         Z = np.matmul(W, A) + b
         if i < L:
-            A = np.tanh(A)
+            A = np.tanh(Z)
             D = np.random.rand(A.shape[0], A.shape[1])
             D = np.where(D < keep_prob, 1, 0)
-            A = np.multiply(D, A)
+            A = np.multiply(A, D)
             A = A / keep_prob
-            cache["D" + str(i)] = D
+            cache['D' + str(i)] = D
         else:
             A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
-            cache["A" + str(i)] = A
+            cache['A' + str(i)] = A
     return cache
