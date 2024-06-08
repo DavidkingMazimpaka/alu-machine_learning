f32b9c9dfbec8a43a67c95a1651472bb25635080
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-06-08 21:16:15 +0200

    12-learning rate

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
-""" learning_rate_decay with tensorflow """
+""" Learning rate decay with tensorflow
+"""
+
 
 import tensorflow as tf
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
-    """ creates the learning rate using inverse time decay in tensorflow:
-    Args: 
-    alpha: is the original learning rate
-    decay_rate: is the weight used to determine the rate at which alpha will decay
-    global_step: is the number of passes of gradient descent that have elapsed
-    decay_step: is the number of passes of GD
-    the learning rate decay should occur in a stepwise fashion
-    Returns: the learning rate decay operation"""
-    return tf.train.inverse_time_decay(alpha, global_step, decay_step, 
-                                       decay_rate, staircase_step=True)
+    """ creates a learning rate decay operation in tensorflow using
+        inverse time decay
+
+    Args:
+        alpha (float): original learning rate
+        decay_rate (float): weight used to determine the rate at
+        which alpha will decay
+        global_step (int): number of passes of gradient descent
+        that have elapsed
+        decay_step (int): number of passes of gradient descent that
+        should occur before alpha is decayed further
+    """
+    return tf.train.inverse_time_decay(alpha, global_step, decay_step,
+                                       decay_rate, staircase=True)
