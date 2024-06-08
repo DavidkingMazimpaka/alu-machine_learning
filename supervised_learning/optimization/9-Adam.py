eefee7eb27e8459db1dfc1bc486db21e3fb17c9a
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-06-08 20:59:19 +0200

    9-adam

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -14,7 +14,7 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     v (np.ndarray): the previous first moment of var
     s (np.ndarray): the previous second moment of var
     t (int): the time step
-    Returns: np.ndarray: the updated variable and the new moment, respectively"""
+    Returns: np.ndarray: the updated variable"""
     v = beta1 * v + (1 - beta1) * grad
     v_corrected = v / (1 - (beta1 ** t))
     s = beta2 * s + (1 - beta2) * (grad ** 2)
