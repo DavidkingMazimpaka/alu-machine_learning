2ac3ec81bdcc7999d54a8c55a8d3f121c782b118
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-06-08 21:18:19 +0200

    13-batch

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -5,17 +5,25 @@ import numpy as np
 
 
 def batch_norm(Z, gamma, beta, epsilon):
-    """normalizes an unactivated output of a neural network using batch normalization:
+    """ normalizes an unactivated output of a neural network using batch
+    normalization
+
     Args:
-    Z is a numpy.ndarray of shape (m, n) that should be normalized
-    m is the number of data points
-    n is the number of features in Z
-    gamma is a numpy.ndarray of shape (1, n) containing the scales for Z
-    beta is a numpy.ndarray of shape (1, n)  containing the offsets for Z
-    epsilon is a small number used to avoid division by zero
-    Returns: the normalized Z matrix """
+        Z (numpy.ndarray): matrix to normalize shape (m, n)
+            m: number of data points
+            n: number of features
+        gamma (numpy.ndarray): shape (1, n)
+        contains the scales used for batch normalization
+        beta (numpy.ndarray): shape (1, n)
+        contains the offsets used for batch normalization
+        epsilon (float): small number used to avoid division by zero
+
+    Returns: the normalized Z matrix
+
+    """
+
     mean = Z.mean(axis=0)
-    std = Z.var(axis=0)
+    variance = Z.var(axis=0)
     Z_norm = (Z - mean) / ((variance + epsilon) ** 0.5)
     Z_tilda = gamma * Z_norm + beta
     return Z_tilda
