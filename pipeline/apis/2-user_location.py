9c9d127a70520a241cf651a930a7c7b7ef48b28b
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-06-24 02:22:12 +0200

    user-location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -7,28 +7,18 @@ import requests
 import time
 
 
-def get_user_location(api_url):
-    """
-    Get and print the location of a GitHub user.
-    Args:
-    api_url (str): Full GitHub API URL for the user.
-    Prints:
-    str: User's location, 'Not found', or rate limit message.
-    """
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        user_data = response.json()
-        print(user_data.get('location') or 'None')
-    elif response.status_code == 404:
-        print('Not found')
-    elif response.status_code == 403:
-        reset_time = int(response.headers.get('X-Ratelimit-Reset', 0))
-        minutes_to_reset = max(0, (reset_time - int(time.time())) // 60)
-        print(f'Reset in {minutes_to_reset} min')
-    else:
-        print(f'Error: Status code {response.status_code}')
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: ./2-user_location.py <github_api_user_url>')
-        sys.exit(1)
-    get_user_location(sys.argv[1])
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
+
+    if res.status_code == 403:
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        current_time = int(time.time())
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+        # get remaining rate
+
+    elif res.status_code == 404:
+        print("Not found")
+    elif res.status_code == 200:
+        res = res.json()
+        print(res['location'])
