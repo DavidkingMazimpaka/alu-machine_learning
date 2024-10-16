1fa754dbc0d57641c4818c06f0c77d29ffcb5fa4
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-10-16 19:12:45 +0200

    erro=0

--- unsupervised_learning/dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -10,30 +10,14 @@ import numpy as np
 def P_init(X, perplexity):
     """
     Initializes variables for calculating P affinities in t-SNE
-
-    Parameters:
-        X [numpy.ndarray of shape (n, d)]: dataset
-            n: number of data points
-            d: number of dimensions in each data point
-        perplexity [float]:
-
-    Returns:
-        D [numpy.ndarray of shape (n, n)]
-        P [numpy.ndarray of shape (n, n)]: 
-        betas [numpy.ndarray of shape (n, 1)]:
-        H [float]: 
     """
     n, d = X.shape
-
     # Calculate squared pairwise distances
     D = np.sum(X**2, axis=1).reshape(n, 1) + np.sum(X**2, axis=1) - 2 * np.dot(X, X.T)
     np.fill_diagonal(D, 0)  # Set diagonal to 0
-
     # Initialize P affinities and beta values
     P = np.zeros((n, n))
     betas = np.ones((n, 1))
-
     # Calculate Shannon entropy for the specified perplexity
     H = np.log2(perplexity)
-
     return D, P, betas, H
